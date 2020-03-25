bfff52b3a1a2a47f4e66cfc5812af20eedcf813d
Jorrit Meeuwissen <jorritjeah@gmail.com>
2020-03-25 15:12:24 +0100

    init Benchmark iOS

---
+++ benchmark_ios/benchmark_ios/benchmark_ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  benchmark_ios
+//
+//  Created by Jorrit Meeuwissen on 25-03-20.
+//  Copyright © 2020 Jorrit Meeuwissen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ benchmark_ios/benchmark_ios/benchmark_iosTests/benchmark_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  benchmark_iosTests.swift
+//  benchmark_iosTests
+//
+//  Created by Jorrit Meeuwissen on 25-03-20.
+//  Copyright © 2020 Jorrit Meeuwissen. All rights reserved.
+//
+
+import XCTest
+@testable import benchmark_ios
+
+class benchmark_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
