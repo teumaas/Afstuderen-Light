0abb73dc1b5093da744a83acaf87aa0de8e997f3
Jorrit076 <jorrit-meeuwissen@hotmail.com>
2020-03-27 15:41:08 +0100

    benchmark React

--- benchmarkReact/App.js
+++ benchmarkReact/App.js
@@ -3,112 +3,152 @@
  * https://github.com/facebook/react-native
  *
  * @format
- * @flow
+ * @flow strict-local
  */
-
-import React from 'react';
+import React, {Component} from 'react';
+import * as Timing from './timing';
 import {
-  SafeAreaView,
+  AppRegistry,
   StyleSheet,
-  ScrollView,
-  View,
+  Button,
+  TouchableOpacity,
   Text,
   StatusBar,
+  SafeAreaView,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+export default class App extends Component {
+  state: {
+    time: string,
+    time2: string,
+  };
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      time: 'Press button to calculate',
+      time2: 'Press button to calculate',
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView style={styles.container}>
+          <Button
+            title="gaussLegendre"
+            onPress={() => {
+              console.log('Start');
+              let startTime = Timing.now();
+              for (let i = 0; i < numIters; i += 1) {
+                gaussLegendre(10000000);
+              }
+              let endTime = Timing.now();
+              console.log('Finish');
+              let iterTime = (endTime - startTime) / numIters;
+              console.log(iterTime);
+              this.setState({time: iterTime.toString()});
+            }}
+          />
+          <Text style={styles.textStyle}>{this.state.time}</Text>
+          <Button
+            title="getOneByPi"
+            onPress={() => {
+              console.log('Start');
+              let startTime = Timing.now();
+              for (let i = 0; i < numIters; i += 1) {
+                getOneByPi(10000000);
+              }
+              let endTime = Timing.now();
+              console.log('Finish');
+              let iterTime = (endTime - startTime) / numIters;
+              console.log(iterTime);
+              this.setState({time2: iterTime.toString()});
+            }}
+          />
+          <Text style={styles.textStyle}>{this.state.time2}</Text>
+        </SafeAreaView>
+      </>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   scrollView: {
     backgroundColor: Colors.lighter,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  start: {
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  body: {
-    backgroundColor: Colors.white,
+  btnStyle: {
+    justifyContent: 'center',
+    alignSelf: 'stretch',
+    textAlignVertical: 'center',
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  startTestButton: {
+    marginRight: 40,
+    marginLeft: 40,
+    marginTop: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: '#1E6738',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#fff',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  startTestText: {
+    color: '#fff',
+    textAlign: 'center',
+    paddingLeft: 10,
+    paddingRight: 10,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  textStyle: {
+    color: 'black',
   },
 });
 
-export default App;
+//AppRegistry.registerComponent('App', () => App);
+
+//Pi calc
+
+const numIters = 100;
+
+function gaussLegendre(iterations) {
+  let a = 1.0;
+  let b = 1.0 / Math.sqrt(2);
+  let t = 1.0 / 4.0;
+  let p = 1.0;
+  for (let i = 0; i < iterations; i++) {
+    let aNext = (a + b) / 2;
+    let bNext = Math.sqrt(a * b);
+    let tNext = t - p * Math.pow(a - aNext, 2);
+    let pNext = 2 * p;
+    a = aNext;
+    b = bNext;
+    t = tNext;
+    p = pNext;
+  }
+  return Math.pow(a + b, 2) / (4 * t);
+}
+
+function getOneByPi(k) {
+  let ak = 6.0 - 4 * Math.sqrt(2);
+  let yk = Math.sqrt(2) - 1.0;
+  var ak1;
+  var yk1;
+  for (let i = 0; i < k; i++) {
+    yk1 =
+      (1 - Math.pow(1 - yk * yk * yk * yk, 0.25)) /
+      (1 + Math.pow(1 - yk * yk * yk * yk, 0.25));
+    ak1 =
+      ak * Math.pow(1 + yk1, 4) -
+      Math.pow(2, 2 * i + 3) * yk1 * (1 + yk1 + yk1 * yk1);
+    yk = yk1;
+    ak = ak1;
+  }
+  return ak;
+}
